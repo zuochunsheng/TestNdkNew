48e4cfcd7d02a24782abf0cf83404d562d926ae4
zuochunsheng <zuochunsheng@ledcas.com>
2019-07-17 10:34:08 +0800

    配置一键生成 JNI 头文件工具

---
+++ app/src/main/java/com/huican/test/NativeRenderer.java
@@ -0,0 +1,9 @@
+package com.huican.test;
+
+/**
+ * @author： zcs
+ * @time：2019/7/17 on 10:14
+ * @description：
+ */
+public class NativeRenderer {
+}
---
+++ app/src/main/java/com/huican/test/SampleActivity.java
@@ -0,0 +1,14 @@
+package com.huican.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.huican.test.R;
+
+public class SampleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+    }
+}
